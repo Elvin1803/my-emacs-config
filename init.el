a6468cd879800a39783e977005b2d1583f62b8c6
Elvin <sisavath2003@gmail.com>
2025-02-06 16:44:35 +0100

    Added my emacs config

---
+++ init.el
@@ -0,0 +1,55 @@
+(require 'package)
+(package-initialize)
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("elpa" . "https://elpa.gnu.org/packages/") t)
+
+(setq my/packages
+      '(
+	;; Visual packages
+	spacemacs-theme
+	all-the-icons
+	page-break-lines
+	org-sticky-header
+	rainbow-delimiters
+	;; Code
+	projectile
+	magit
+	company
+	lsp-mode
+	lsp-ui
+	;; Text editing / Navigation
+	avy
+	move-text
+	smartparens
+	yasnippet
+	;; Misc
+	dashboard
+	helm
+	neotree
+	))
+
+(unless (file-exists-p package-user-dir)
+  (package-refresh-contents))
+
+;; Install missing packages
+(dolist (package my/packages)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+(require 'org)
+(org-babel-load-file "~/.emacs.d/config.org")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(yasnippet spacemacs-theme smartparens rainbow-delimiters projectile page-break-lines org-sticky-header neotree move-text magit lsp-ui helm dashboard company avy all-the-icons)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
