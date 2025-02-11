7c61681afa70978f4561f0f06533da5314fe74e8
Elvin <elvin.sisavath@epita.fr>
2025-02-11 16:48:28 +0100

    Added rust config

--- init.el
+++ init.el
@@ -20,6 +20,7 @@
 	company
 	lsp-mode
 	lsp-ui
+	rust-mode
 	;; Text editing / Navigation
 	avy
 	move-text
@@ -48,7 +49,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(git-gutter-fringe git-gutter yasnippet spacemacs-theme smartparens rainbow-delimiters projectile page-break-lines org-sticky-header neotree move-text magit lsp-ui helm dashboard company avy all-the-icons)))
+   '(rust-mode rust-analyzer git-gutter-fringe git-gutter yasnippet spacemacs-theme smartparens rainbow-delimiters projectile page-break-lines org-sticky-header neotree move-text magit lsp-ui helm dashboard company avy all-the-icons)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
