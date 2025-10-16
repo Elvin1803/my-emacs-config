d86a9c3cf0b8367ba8bb7837f5076fd82880c5cc
Elvin <sisavath2003@gmail.com>
2025-10-16 22:29:39 +0200

    init: starting new config for 2026

--- init.el
+++ init.el
@@ -1,79 +1,46 @@
-(require 'package)
-(package-initialize)
+;; Init.el
 
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("elpa" . "https://elpa.gnu.org/packages/") t)
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(setq package-enable-at-startup nil)
 
-(setq my/packages
-      '(
-    ;; Visual packages
-    spacemacs-theme
-    all-the-icons
-    page-break-lines
-    org-sticky-header
-    rainbow-delimiters
-    git-gutter
-    git-gutter-fringe
-    ;; Code
-    projectile
-    cmake-ide
-    nix-mode
-    magit
-    company
-    lsp-mode
-    lsp-ui
-    ;; Text editing / Navigation
-    avy
-    move-text
-    smartparens
-    yasnippet
-    ;; Misc
-    dashboard
-    helm
-    neotree
-    elcord
-    ))
+(if (eq system-type 'windows-nt)
+  (elpaca-no-symlink-mode)
+)
 
-(unless (file-exists-p package-user-dir)
-  (package-refresh-contents))
-
-;; Install missing packages
-(dolist (package my/packages)
-  (unless (package-installed-p package)
-    (package-install package)))
-
-(require 'elcord)
-(elcord-mode)
-
-(require 'org)
-(org-babel-load-file "~/.emacs.d/config.org")
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("95b0bc7b8687101335ebbf770828b641f2befdcf6d3c192243a251ce72ab1692"
-     "aa545934ce1b6fd16b4db2cf6c2ccf126249a66712786dd70f880806a187ac0b"
-     default))
- '(package-selected-packages
-   '(all-the-icons avy cmake-ide company dashboard elcord git-gutter
-                   git-gutter-fringe helm lsp-java lsp-ui magit
-                   monokai-theme move-text neotree nix-mode
-                   org-sticky-header page-break-lines projectile
-                   rainbow-delimiters rust-analyzer rust-mode
-                   smartparens standard-theme standard-themes
-                   yasnippet))
- '(whitespace-display-mappings
-   '((space-mark 32 [183] [46]) (space-mark 160 [164] [95])
-     (tab-mark 9 [187 9] [92 9]))))
-
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(defvar elpaca-installer-version 0.11)
+(defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
+(defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
+(defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
+(defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
+                              :ref nil :depth 1 :inherit ignore
+                              :files (:defaults "elpaca-test.el" (:exclude "extensions"))
+                              :build (:not elpaca--activate-package)))
+(let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
+       (build (expand-file-name "elpaca/" elpaca-builds-directory))
+       (order (cdr elpaca-order))
+       (default-directory repo))
+  (add-to-list 'load-path (if (file-exists-p build) build repo))
+  (unless (file-exists-p repo)
+    (make-directory repo t)
+    (when (<= emacs-major-version 28) (require 'subr-x))
+    (condition-case-unless-debug err
+        (if-let* ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
+                  ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
+                                                  ,@(when-let* ((depth (plist-get order :depth)))
+                                                      (list (format "--depth=%d" depth) "--no-single-branch"))
+                                                  ,(plist-get order :repo) ,repo))))
+                  ((zerop (call-process "git" nil buffer t "checkout"
+                                        (or (plist-get order :ref) "--"))))
+                  (emacs (concat invocation-directory invocation-name))
+                  ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
+                                        "--eval" "(byte-recompile-directory \".\" 0 'force)")))
+                  ((require 'elpaca))
+                  ((elpaca-generate-autoloads "elpaca" repo)))
+            (progn (message "%s" (buffer-string)) (kill-buffer buffer))
+          (error "%s" (with-current-buffer buffer (buffer-string))))
+      ((error) (warn "%s" err) (delete-directory repo 'recursive))))
+  (unless (require 'elpaca-autoloads nil t)
+    (require 'elpaca)
+    (elpaca-generate-autoloads "elpaca" repo)
+    (let ((load-source-file-function nil)) (load "./elpaca-autoloads"))))
+(add-hook 'after-init-hook #'elpaca-process-queues)
+(elpaca `(,@elpaca-order))
