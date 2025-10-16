9b7b3039ddbafab4738b9cd4e7873485faf13e0d
Elvin <sisavath2003@gmail.com>
2025-10-16 22:06:32 +0200

    feat: finalized version for 2025

--- init.el
+++ init.el
@@ -17,12 +17,12 @@
     git-gutter-fringe
     ;; Code
     projectile
+    cmake-ide
     nix-mode
     magit
     company
     lsp-mode
     lsp-ui
-    rust-mode
     ;; Text editing / Navigation
     avy
     move-text
@@ -32,6 +32,7 @@
     dashboard
     helm
     neotree
+    elcord
     ))
 
 (unless (file-exists-p package-user-dir)
@@ -42,6 +43,9 @@
   (unless (package-installed-p package)
     (package-install package)))
 
+(require 'elcord)
+(elcord-mode)
+
 (require 'org)
 (org-babel-load-file "~/.emacs.d/config.org")
 
@@ -50,12 +54,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("95b0bc7b8687101335ebbf770828b641f2befdcf6d3c192243a251ce72ab1692"
+     "aa545934ce1b6fd16b4db2cf6c2ccf126249a66712786dd70f880806a187ac0b"
+     default))
  '(package-selected-packages
-   '(all-the-icons avy company dashboard elcord git-gutter
+   '(all-the-icons avy cmake-ide company dashboard elcord git-gutter
                    git-gutter-fringe helm lsp-java lsp-ui magit
-                   move-text neotree nix-mode org-sticky-header
-                   page-break-lines projectile rainbow-delimiters
-                   rust-analyzer rust-mode smartparens spacemacs-theme
+                   monokai-theme move-text neotree nix-mode
+                   org-sticky-header page-break-lines projectile
+                   rainbow-delimiters rust-analyzer rust-mode
+                   smartparens standard-theme standard-themes
                    yasnippet))
  '(whitespace-display-mappings
    '((space-mark 32 [183] [46]) (space-mark 160 [164] [95])
