782de752cb0581168b694465ddc12aa173ecc532
Elvin <sisavath2003@gmail.com>
2025-10-17 00:41:30 +0200

    feat: added custom theme

--- early-init.el
+++ early-init.el
@@ -1,1 +1,18 @@
+;;; early-init.el
+
 (setq package-enable-at-startup nil)
+
+;; minimal UI
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(setq inhibit-startup-message t)
+
+;; Deactivate bell sound
+(setq ring-bell-function 'ignore)
+
+;; Prevent Emacs from doing backups
+(setq make-backup-files nil)
+
+;;; early-init.el ends here
